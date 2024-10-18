08a6cadcad874845b4aa6256dda01f525003e8c7
kongchenglc <kongchenglc@gmail.com>
2024-10-18 00:31:30 -0400

    feat: use inference API

--- routes/gptchat.js
+++ routes/gptchat.js
@@ -1,14 +1,43 @@
-import { pipeline } from '@xenova/transformers';
+// import { pipeline } from '@xenova/transformers';
+// import Router from 'koa-router';
+// const router = new Router();
+
+// router.prefix('/gptchat')
+
+// router.get('/', async function (ctx, next) {
+//   const userInput = ctx.query.message
+//   const model = await pipeline('text-generation', 'Xenova/gpt2');
+//   const response = await model(userInput || 'Input Nothing');
+//   ctx.body = response?.[0]?.generated_text
+// })
+
+// export default router
+
+import { HfInference } from "@huggingface/inference";
 import Router from 'koa-router';
 const router = new Router();
+
+const inference = new HfInference("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");
+
+async function query(data) {
+  let result = ''
+  for await (const chunk of inference.chatCompletionStream({
+    model: "meta-llama/Llama-3.2-1B-Instruct",
+    messages: [{ role: "user", content: "What is the capital of France?" }],
+    max_tokens: 500,
+  })) {
+    console.log(chunk.choices[0]?.delta)
+    result += chunk.choices[0]?.delta?.content || ""
+  }
+  return result;
+}
 
 router.prefix('/gptchat')
 
 router.get('/', async function (ctx, next) {
   const userInput = ctx.query.message
-  const model = await pipeline('text-generation', 'Xenova/gpt2');
-  const response = await model(userInput || 'Input Nothing');
-  ctx.body = response?.[0]?.generated_text
+  const response = await query({"inputs": "Can you please let us know more details about your "})
+  ctx.body = response
 })
 
 export default router
